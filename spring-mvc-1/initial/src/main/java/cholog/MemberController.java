7e488d1d41430a4a50df1d34ebdc7b4e93ba9865
hyun <sarahs19991115@gmail.com>
2024-09-15 21:57:35 +0900

    feat($data response): 쿼리 파라미터로 name 요청이 들어왔을 때 hello.html에서 사용할 수 있도록 함 test($data response): 위 내용 테스트 완료

--- spring-mvc-1/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/initial/src/main/java/cholog/MemberController.java
@@ -2,15 +2,22 @@ package cholog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.ui.Model;
 
 @Controller
 public class MemberController {
 
     @GetMapping("hello")
-    public String world() {
+    public String world(@RequestParam(value="name", required = false) String name, Model model) {
         // TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요.
         // TODO: 쿼리 파라미터로 name 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 하세요.
-        return "static";
+        model.addAttribute("name", name);
+        if (name == null)
+            return "static";
+        else
+            return "hello";
     }
 
     public Person json() {
