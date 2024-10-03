0e9e301d0c78a04802d4ad1075ce11638ef74720
hyun <sarahs19991115@gmail.com>
2024-10-03 15:11:34 +0900

    feat($KeyHolder): insertWithKeyHolder() 함수 완성
    test($KeyHolder): 해당 기능 테스트 통과

--- spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
@@ -50,10 +50,17 @@ public class UpdatingDAO {
         String sql = "insert into customers (first_name, last_name) values (?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        //todo : keyHolder에 대해 학습하고, Customer를 저장후 저장된 Customer의 id를 반환하기
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+            ps.setString(1, customer.getFirstName());
+            ps.setString(2, customer.getLastName());
+            return ps;
+        }, keyHolder);
 
         Long id = keyHolder.getKey().longValue();
 
-        return keyHolder.getKey().longValue();
+        return id;
     }
 }
