ab45653227a59c0ebf6a01cc53c609d56b1bae93
hyun <sarahs19991115@gmail.com>
2024-10-03 14:14:00 +0900

    feat($Select): getLastName() 함수 완성
    test($Select): 해당 기능 테스트 통과

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -38,8 +38,8 @@ public class QueryingDAO {
      * public <T> T queryForObject(String sql, Class<T> requiredType, @Nullable Object... args)
      */
     public String getLastName(Long id) {
-        //TODO : 주어진 Id에 해당하는 customers의 lastName을 반환
-        return null;
+        String lastName = jdbcTemplate.queryForObject("select last_name from customers where id = ?", String.class, id);
+        return lastName;
     }
 
     /**
