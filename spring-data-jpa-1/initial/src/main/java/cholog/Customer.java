cf2edb1e1545c781d3b27269e6623f71e21cc234
nuyhhyun <sarahs19991115@gmail.com>
2024-10-24 13:01:13 +0900

    feat(EntityMapping): $Customer 엔티티 매핑 기능 완성

--- spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
@@ -1,8 +1,17 @@
 package cholog;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
 
